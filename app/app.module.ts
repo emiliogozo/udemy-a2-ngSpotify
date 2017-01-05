2fb1899113f5c1e0f0b49d0a88ea3570a137fe4c
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-01-06 00:20:38 +0800

    feature: search component and service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +14,9 @@ import { routing } from './app.routing';
 @NgModule({
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule,
+    HttpModule
   ],
   declarations: [
     AppComponent,
